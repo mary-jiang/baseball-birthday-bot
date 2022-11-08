bef20661d9b9e144c511ecccc199cf695fd947ac
Mary Jiang <mary.jiang2020@gmail.com>
2022-11-08 15:12:05 -0500

    wrote function to get list of all birthday boys

--- birthday_scraper.py
+++ birthday_scraper.py
@@ -1,6 +1,22 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://www.baseball-reference.com/friv/birthdays.cgi"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="div_birthday_stats")
+
+def list_of_names():
+    """
+    Returns a list of names of players whose birthday is today
+    """
+    name_elements = results.find_all(class_="left", attrs={"data-stat": "player"})
+
+    names = []
+    for name_element in name_elements:
+        names.append(name_element.text)
+    names.pop(0) # the first element is always "Name", do not want that
+
+    return names 
